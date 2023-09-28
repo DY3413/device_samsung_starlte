af87c2736072f3cb4e67bbc6d6f02bf635e71e91
DY3413 <milosix3413@gmail.com>
2023-09-28 19:23:45 +0900

    starlte: Set vendor security patch level
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,5 +15,8 @@ TARGET_KERNEL_CONFIG := exynos9810-starlte_defconfig
 # Screen density
 TARGET_SCREEN_DENSITY := 560
 
+# Vendor security patch level
+VENDOR_SECURITY_PATCH := 2022-03-01
+
 # Inherit from proprietary files
 include vendor/samsung/starlte/BoardConfigVendor.mk
