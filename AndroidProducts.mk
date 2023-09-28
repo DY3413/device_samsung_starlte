b64b3a9a70716ba2794f18b454b729fe87538cc1
DY3413 <milosix3413@gmail.com>
2023-09-28 16:39:42 +0900

    starlte: Initialize device folder
    Signed-off-by: DY3413 <milosix3413@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_starlte.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_starlte-user \
+    aosp_starlte-userdebug \
+    aosp_starlte-eng
---
+++ aosp_starlte.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from starlte device
+$(call inherit-product, device/samsung/starlte/device.mk)
+
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_starlte
+PRODUCT_DEVICE := starlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G960F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
